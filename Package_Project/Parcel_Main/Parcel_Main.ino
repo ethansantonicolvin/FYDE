58af4d3f09f9be6169af7286ec98b0e0d9250489
Ethan Santoni-Colvin <ethan.santonicolvin@utexas.edu>
2018-03-09 16:38:09 -0600

    LED output/threshold added
    Need to experiment with different thresholds/moving average sample numbers and get time figured out. Commenting out Serial.print() commands seems to speed up data collection and greatly lower the force needed to trigger.

--- Package_Project/Parcel_Main/Parcel_Main.ino
+++ Package_Project/Parcel_Main/Parcel_Main.ino
@@ -10,17 +10,23 @@ struct accelData{
 
 #define LED_PIN 13
 #define RANGESCALAR 2048.0
+#define nMovingAvg 10
+#define maxAllowedAccel 8.5
 
 MPU6050 accelgyro;
 struct accelData getAccelValues(void);
-
-bool blinkState = false;
-
+double movingAvgArr[nMovingAvg];
+int sampleNumber = 0;
+double movingAvg;
+boolean ledState = false;
 
 //****************************SETUP**********************************************
 
 
 void setup() {
+    //init onboard LED
+    pinMode(LED_PIN, OUTPUT);
+      
     // join I2C bus (I2Cdev library doesn't do this automatically)
     #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
         Wire.begin();
@@ -38,7 +44,7 @@ void setup() {
     Serial.println("Testing device connections...");
     Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
 
-    // setting offsets
+    // setting offsets to calibrate sensor
     accelgyro.setXGyroOffset(34);
     accelgyro.setYGyroOffset(68);
     accelgyro.setZGyroOffset(12);
@@ -52,12 +58,30 @@ void setup() {
 
 void loop() {
     struct accelData data = getAccelValues();
-    Serial.print(data.accelX);Serial.print("\t");
-    Serial.print(data.accelY);Serial.print("\t");
-    Serial.print(data.accelZ);Serial.print("\t");
-    Serial.print(getMagnitude(data));Serial.print("\n");
+    //Serial.print(data.accelX);Serial.print("\t");
+    //Serial.print(data.accelY);Serial.print("\t");
+    //Serial.print(data.accelZ);Serial.print("\t");
+    //Serial.print(getMagnitude(data));Serial.print("\t"); 
 
-  
+    if(sampleNumber < 5){ // don't do moving average until we have 5 values
+      movingAvgArr[sampleNumber] = getMagnitude(data);
+      movingAvg = getMagnitude(data);
+    }
+    else{
+      movingAvgArr[sampleNumber % 5] = getMagnitude(data);
+      movingAvg = (movingAvgArr[0] + movingAvgArr[1] + movingAvgArr[2] + movingAvgArr[3] + movingAvgArr[4])/5.0;        
+    }
+
+    Serial.print(movingAvg);Serial.print("\n");
+    
+    if(movingAvg > maxAllowedAccel){
+      onboardLEDToggle();
+      delay(2000);
+      onboardLEDToggle();
+      sampleNumber = -1; //re-populate array for next 5 loops  
+    }
+    
+    sampleNumber++;  
 }
 
 
@@ -80,6 +104,13 @@ double getMagnitude(struct accelData a){
   return sqrt(sq(a.accelX) + sq(a.accelY) + sq(a.accelZ));
 }
 
+void onboardLEDToggle(void){
+  if(!ledState)
+    digitalWrite(LED_PIN, HIGH);
+  else
+    digitalWrite(LED_PIN, LOW);
+  ledState = !ledState;
+}
 
 
 
